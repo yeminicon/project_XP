fbfe76f0bd82ff67142856fde0b757df42c30ab0
yeminicon <akinyele.adeyemi9005@gmail.com>
2023-11-17 08:38:30 +0100

    remove types

--- src/types.ts
+++ src/types.ts
@@ -9,46 +9,6 @@ export type dbConfigs = {
   port: number;
 };
 
-export enum ResourceLockStatus {
-  ACTIVE = 'active',
-  INACTIVE = 'inactive',
-}
-
-export enum ResourceEnumPrefix {
-  BANKTRANSFER = 'bankTransfer',
-}
-
-export interface BaniVirtualAccountResponse {
-  message: string;
-  status: boolean;
-  status_code: number;
-  payment_reference: string;
-  holder_account_number: string;
-  amount: string;
-  payment_ext_reference: string;
-  account_type: string;
-  account_name: string;
-  custom_data: any | null;
-}
-
-export interface BaniEnquiryAccountResponse {
-  message: string;
-  status: boolean;
-  status_code: number;
-  data: {
-    account_trade_name: string;
-    fiat_wallet_details: {
-      available_balance: string;
-      currency: string;
-      modified_date: string;
-      pub_date: string;
-      total_balance: string;
-      wallet_account_ref: string;
-      wallet_alias: string;
-    }[];
-  };
-}
-
 declare global {
   interface AuthRequest extends Request {
     user: User;
